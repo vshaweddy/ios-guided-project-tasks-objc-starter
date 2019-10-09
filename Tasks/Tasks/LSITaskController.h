e8bade9d7f388fdbfd8eccf8166214c377f465bd
Paul Solt <PaulSolt@gmail.com>
2019-10-09 13:56:17 -0400

    Started to work on TaskController

---
+++ Tasks/Tasks/LSITaskController.h
@@ -0,0 +1,25 @@
+//
+//  LSITaskController.h
+//  Tasks
+//
+//  Created by Paul Solt on 10/9/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSITaskController : NSObject
+
+// tasks array  (private)
+
+// add task
+// remove task
+// count
+// taskAtIndex
+
+// complete task (more at LSITask level)
+
+
+
+
+@end
---
+++ Tasks/Tasks/LSITaskController.m
@@ -0,0 +1,22 @@
+//
+//  LSITaskController.m
+//  Tasks
+//
+//  Created by Paul Solt on 10/9/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import "LSITaskController.h"
+
+// Class Extension
+@interface LSITaskController ()
+
+// Private properties
+@property NSArray *tasks;
+
+@end
+
+
+@implementation LSITaskController
+
+@end
